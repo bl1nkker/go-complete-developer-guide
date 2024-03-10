a0afaefcd6689cec377c5047c86988cd1339aafe
bl1nkker <nurovich14@gmail.com>
2024-03-10 23:37:16 +0500

    slices and for loops

--- cards/main.go
+++ cards/main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{newCard(), newCard()}
+	// Returns a new slice
+	cards = append(cards, "Six of Spades")
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string{
