0f4f2c873b142eb5339a99116bd197ccc1e99158
bl1nkker <nurovich14@gmail.com>
2024-03-11 21:00:07 +0500

    added toString method for deck

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type deck []string
 
@@ -29,4 +32,10 @@ func newDeck() deck{
 		}
 	}
 	return cards
+}
+
+func (d deck) toString() string{
+	var result string
+	result = strings.Join([]string(d), ",")
+	return result
 }
